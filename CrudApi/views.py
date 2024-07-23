8937bc894a435b59f78ac2c2569484b90a8caec0
Andronicus Lepcha <andronicus.lepcha@non.keysight.com>
2024-07-23 10:49:17 +0530

    Retrieve API done

--- CrudApi/views.py
+++ CrudApi/views.py
@@ -22,8 +22,21 @@ def create_data(request):
     return HttpResponse("API create_data added the new data in the DB and REDIS !")
 
 # RETRIEVE
-@api_view(['GET'])
+@api_view(['POST'])
 def retrieve_data(request):
+    
+    payload=request.data
+    inital_page = payload.get('page', 1)
+    items_per_page = 20
+    
+    try:
+        page = int(inital_page)
+    except ValueError:
+        page = 1
+    
+    start_index = (page - 1) * items_per_page
+    end_index = start_index + items_per_page - 1
+    
     try:
         redis_connection=Redis(host='redis', port=6379, db=1)
         print("connection success!")
@@ -31,12 +44,12 @@ def retrieve_data(request):
         print("Exception",e)
         print("Redis connection failed ! ")
     
-    redis_data_chunk = redis_connection.lrange("media_files_list",0,100) # try to fetch the data from the redis key 
+    redis_data_chunk = redis_connection.lrange("media_files_list",start_index,end_index)
     if not redis_data_chunk:
         media_data=dummy_model_data.objects.all().values()
         for item in media_data:
             redis_connection.rpush("media_files_list", json.dumps(item))
-        redis_data_chunk = redis_connection.lrange("media_files_list",0,100)
+        redis_data_chunk = redis_connection.lrange("media_files_list",start_index,end_index)
     redis_data = [json.loads(item) for item in redis_data_chunk]
     
     return Response(redis_data)
